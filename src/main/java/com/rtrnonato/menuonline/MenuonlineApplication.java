19a3b3f05448f50121679244f99d991f14b4146b
Artur <rtrnonato@gmail.com>
2024-04-18 16:32:51 -0300

    initial commit

---
+++ src/main/java/com/rtrnonato/menuonline/MenuonlineApplication.java
@@ -0,0 +1,13 @@
+package com.rtrnonato.menuonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtrnonato/menuonline/MenuonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtrnonato.menuonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
